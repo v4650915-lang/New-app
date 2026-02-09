8b452d2ffec30e40f55174cdd2764c4b0ed8082a
Vladimir <vladimir@example.com>
2026-02-09 22:21:20 +0300

    Secure Calculator + Add CNC Favicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3001,
+    host: '0.0.0.0',
+  },
+  preview: {
+    port: 3001,
     host: '0.0.0.0',
   },
   plugins: [react()],
